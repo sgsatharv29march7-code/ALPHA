1ed64d8063c0086dd246be51a946612a0f08e07a
atharvsharma <sgs.atharv.29march.7@gmail.com>
2026-08-11 01:10:24 +0530

    new program

---
+++ P1.c
@@ -0,0 +1,11 @@
+//Q1: Write a program to input two numbers and display their sum.
+#include <stdio.h>
+
+int main(){
+    int num1,num2,sum;
+    printf("enter two numbers");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("sum = %d",sum);
+    return 0;
+}
